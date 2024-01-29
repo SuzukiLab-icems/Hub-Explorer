64a3bbdb1ea854fb07714e743260eb4a2f59d3f0
yuki noguchi <131225436+nyuhki@users.noreply.github.com>
2024-01-29 14:39:16 +0900

    Update Hub-Explorer.sh
    Describe how to do test run.

--- v1.0.0/Hub-Explorer.sh
+++ v1.0.0/Hub-Explorer.sh
@@ -1,4 +1,5 @@
 #!/usr/sh
+
 ###########################################################################
 #Hub-Explorer_v1.0.0/Hub-Explorer.sh
 #
@@ -14,6 +15,25 @@
 ##CMD for generating figures
 #Figure.5E-5G: sh ./Hub-Explorer_v1.0.0/Hub-Explorer.sh -i IO -m mtx_from_Figure.4B-C_testicular_gene_expr_table_for_Figure.5.csv -l list_modified_from_Figure.5C_summary_table_of_spermatid_specific_RD3_interactors.csv -a annotation_file_from_scanpy_var.csv -o manual -k 4 -t Rd3
 #Figure.S5D-S5E: sh ./Hub-Explorer_v1.0.0/Hub-Explorer.sh -i IO -m mtx_from_Figure.4B-C_testicular_gene_expr_table_for_Figure.5.csv -l list_from_Figure.S5C.csv -a annotation_file_from_scanpy_var.csv -o manual -k 7 -t None
+#
+#
+##For test run
+#You can run Hub-Explorer by typing 'sh Hub-Explorer_v1.0.0/Hub-Explorer.sh -i demo_data -m demo_matrix.csv -l demo_list.csv -a annotation_file.csv -o manual -k 2 -t None' in 'Hub-Explorer' directory. Please type pwd and confirm your current directory is 'Hub-Explorer.'
+#
+#[Directory Architecture]
+#Hub-Explorer
+#	|
+#	|-Hub-Explorer_v1.0.0
+#	|	|-source codes..
+#	|
+#	|-demo_data
+#		|-annotation_file.csv
+#		|-demo_list.csv
+#		|-demo_matrix.csv
+#		|-goa_mouse.gaf.gz
+#		|-go-basic.obo
+#
+#If you want to change the directory, pls fix line.107 for your preference.
 ###########################################################################
 
 input_directory=""
